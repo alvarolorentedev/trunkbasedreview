059e0ebb6d2163150987e49c566370799c3a0b08
kanekotic <me@kanekotic.com>
2022-10-24 08:50:35 +0200

    Alvaro | added dinamic load

--- vs-code/src/extension.ts
+++ vs-code/src/extension.ts
@@ -24,21 +24,23 @@ export function activate(context: vscode.ExtensionContext) {
 	// A `CommentController` is able to provide comments for documents.
 	const commentController = vscode.comments.createCommentController('comment-sample', 'Comment API Sample');
 	context.subscriptions.push(commentController);
-	vscode.workspace.openTextDocument(vscode.Uri.file('C:\\Users\\alvaro.perez\\OneDrive - ClimatePartner GmbH\\Desktop\\review.json')
-	).then((document) => {
-		let {threads} = JSON.parse(document.getText());
-		threads.forEach((element:any) => {
-			commentController.createCommentThread(
-				vscode.Uri.file(element.file), 
-				new vscode.Range(element.range.startLine,element.range.startCharacter,element.range.endLine,element.range.endCharacter), 
-				element.comments.map((comment: any) => new NoteComment(comment.text, vscode.CommentMode.Preview, { name: comment.owner })))
-		});
-		console.log(threads)
-	});
-	commentController.createCommentThread(vscode.Uri.file('C:\\Users\\alvaro.perez\\OneDrive - ClimatePartner GmbH\\Desktop\\test.txt'), new vscode.Range(0,0,0,0), [
-	 	new NoteComment("awesome", vscode.CommentMode.Preview, { name: 'vscode' }, undefined, undefined)
-	 ])
-	
+	vscode.workspace.workspaceFolders?.forEach(
+		folder => {
+			const reviewFile = vscode.Uri.joinPath(folder.uri, '.review',  'unresolved.json')
+			vscode.workspace.openTextDocument(vscode.Uri.file(reviewFile.path)
+			).then((document) => {
+				let {threads} = JSON.parse(document.getText());
+				threads.forEach((element:any) => {
+					commentController.createCommentThread(
+						vscode.Uri.file(element.file), 
+						new vscode.Range(element.range.startLine,element.range.startCharacter,element.range.endLine,element.range.endCharacter), 
+						element.comments.map((comment: any) => new NoteComment(comment.text, vscode.CommentMode.Preview, { name: comment.owner })))
+				});
+				console.log(threads)
+			});
+		}
+	)
+
 	// A `CommentingRangeProvider` controls where gutter decorations that allow adding comments are shown
 	commentController.commentingRangeProvider = {
 		provideCommentingRanges: (document: vscode.TextDocument, token: vscode.CancellationToken) => {
